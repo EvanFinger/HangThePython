8b9bd749fed5b36c098143ff5ac5380e1868eb62
Evan Finger <evanfinger@outlook.com>
2023-08-11 22:45:33 -0400

    Finished the simple game

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import os
 
 from random_word import RandomWords
 
-
+global difficulty
+difficulty = 0
 def initProj():
     print("Lets hang the python!! Choose a difficulty: 1, 2, or 3.")
 
 
 def getDiff():
     global difficulty
-    difficulty = 0
     difficulty = int(input("Selected Difficulty: "))
 
 
 initProj()
 
-"""
+
 while True:
     if difficulty < 1 or difficulty > 3:
         print("That option is unavailable. Try again.")
@@ -23,7 +23,7 @@ while True:
     else:
         print("You have selected difficulty level " + str(difficulty))
         break
-"""
+
 global error_count
 error_count = 0
 
@@ -279,11 +279,12 @@ def drawSnake(stage):
     ]
     for element in range(0,15):
         print(*snakelist[stage][element])
-
+        
 
 def gameCycle():
     global error_count
-    print(error_count)
+
+    print(wordToGuess)
     drawSnake(error_count)
     print("MAKE A GUESS: ", end="")
     for element in range(0, len(wordToGuess)):
@@ -300,6 +301,7 @@ def gameCycle():
 
     counter = 0  # tracks index off guess in word
     correct = False
+
     if len(u_input) == 1:
         if u_input not in guessed:
             guessed.append(u_input)
@@ -309,8 +311,15 @@ def gameCycle():
                     char_positions.append(counter)
                     correct = True
             counter += 1
+    elif len(u_input) > 1:
+        if u_input in wordToGuess and len(u_input) == len(wordToGuess):
+            for element in range(0, len(wordToGuess)):
+                if element not in char_positions:
+                    char_positions.append(element)
+            correct = True
+
     if not correct:
-        error_count += 1
+        error_count += 1 * difficulty
 
 
 while (not len(char_positions) == len(wordToGuess)) and not(error_count == 12):
